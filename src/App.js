562a7c4b155ece83058a1485c7342644253f25ea
Alapottra <alapottratrojon@gmail.com>
2022-05-10 12:56:24 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="max-w-7xl mx-auto px-12">
 
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
 
     </div>
