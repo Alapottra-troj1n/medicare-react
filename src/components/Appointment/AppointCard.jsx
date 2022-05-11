885314c7adae90e1c4f22c901968aa3d36c6faf3
Alapottra <alapottratrojon@gmail.com>
2022-05-11 17:04:19 +0600

    added modal

--- src/components/Appointment/AppointCard.jsx
+++ src/components/Appointment/AppointCard.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
-const AppointCard = ({ service }) => {
+const AppointCard = ({ service,setTreatment }) => {
     const { name, slots } = service;
     return (
         <div>
-            <div class="card lg:max-w-lg bg-base-100 shadow-xl p-5">
-                <div class="card-body text-center">
-                    <h2 class="text-2xl font-semibold text-center">{name}</h2>
-                    <p>{
+            <div className="card lg:max-w-lg bg-base-100 shadow-xl p-5">
+                <div className="card-body text-center">
+                    <h2 className="text-2xl font-semibold text-center text-secondary">{name}</h2>
+                    <p className="text-lg font-medium">{
                         slots.length > 0
                             ? <span>{slots[0]}</span>
                             : <span className='text-red-500'>Try another date.</span>
                     }</p>
                     <p>{slots.length ? slots.length + ' spaces available' : slots.length + ' space available'}</p>
-                    <div class="card-actions justify-center">
-                        <button class="btn btn-primary">Book Appointment</button>
+                    <div className="card-actions justify-center">
+                        <label for="booking-modal" className="btn modal-button btn btn-primary bg-gradient-to-r from-secondary to-primary uppercase font-bold text-white hover:bg-gradient-to-r hover:from-primary hover:to-secondary transition-all ease-in hover:border-0 border-0" disabled={slots.length===0} onClick={()=> setTreatment(service)} >Book Appointment</label>
+
                     </div>
                 </div>
             </div>
--- src/components/Appointment/AppointmentPage.jsx
+++ src/components/Appointment/AppointmentPage.jsx
@@ -4,10 +4,12 @@ import chair from '../../assets/images/chair.png';
 import 'react-day-picker/dist/style.css';
 import { format } from 'date-fns';
 import AppointCard from './AppointCard';
+import TreatmentModal from './TreatmentModal';
 
 const AppointmentPage = () => {
-    const [date , setDate] = useState(new Date())
+    const [date , setDate] = useState(new Date());
     const [services , setServices] = useState([]);
+    const [treatment , setTreatment] = useState(null)
      useEffect(() => {
         
         const getServices = async() =>{
@@ -23,9 +25,10 @@ const AppointmentPage = () => {
 
     return (
         <div className='container mx-auto'>
-            <div class="hero min-h-screen">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <img src={chair} alt="Dentist Chair" class="max-w-xl rounded-lg shadow-2xl ml-10" />
+            <TreatmentModal treatment={treatment} />
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={chair} alt="Dentist Chair" className="max-w-xl rounded-lg shadow-2xl ml-10" />
                     <div>
                         <DayPicker
                            mode="single"
@@ -34,13 +37,18 @@ const AppointmentPage = () => {
                         />
                         
                     </div>
+               
                 </div>
             </div>
 
             <div className="appointmentCards">
-                   <h4 className="text-primary text-center text-2xl font-bold my-5"> <span className="font-semibold">Appointment Available For :</span>  {format(date, 'PP')}</h4>
+                 {date ?   <h4 className="text-primary text-center text-2xl font-bold my-5"> <span className="font-semibold">Appointment Available For :</span>  
+                   
+                   {format(date, 'PP')}</h4> :<h4 className="text-primary text-center text-2xl font-bold my-5">Please Pick a Date </h4> }
+
+                   
                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
-                   {services.map(service => <AppointCard service={service} key={service._id} />)}
+                   {services.map(service => <AppointCard service={service} setTreatment={setTreatment} key={service._id} />)}
                    </div>
                 </div>
 
